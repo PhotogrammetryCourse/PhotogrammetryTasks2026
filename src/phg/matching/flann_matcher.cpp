b968435efaf3f6b2e89ce220ab62b8cd42f2b0e3
IrDIE <110756720+IrDIE@users.noreply.github.com>
2026-03-15 15:27:13 +0300

    add task02 files

--- src/phg/matching/flann_matcher.cpp
+++ src/phg/matching/flann_matcher.cpp
@@ -6,8 +6,10 @@
 phg::FlannMatcher::FlannMatcher()
 {
     // параметры для приближенного поиска
-//    index_params = flannKdTreeIndexParams(TODO);
-//    search_params = flannKsTreeSearchParams(TODO);
+
+   index_params = flannKdTreeIndexParams(4);
+   search_params = flannKsTreeSearchParams(32);
+
 }
 
 void phg::FlannMatcher::train(const cv::Mat &train_desc)
@@ -17,5 +19,34 @@ void phg::FlannMatcher::train(const cv::Mat &train_desc)
 
 void phg::FlannMatcher::knnMatch(const cv::Mat &query_desc, std::vector<std::vector<cv::DMatch>> &matches, int k) const
 {
-    throw std::runtime_error("not implemented yet");
+    cv::Mat indices(query_desc.rows, k, CV_32SC1);
+    cv::Mat dists(query_desc.rows, k, CV_32FC1);
+
+    // do k-nearest neighbor search
+    flann_index->knnSearch(query_desc, indices, dists, k, *search_params);
+
+    // store results
+    matches.resize(query_desc.rows);
+    for (int i = 0; i < query_desc.rows; ++i) {
+        matches[i].reserve(k);
+        for (int j = 0; j < k; ++j) {
+            matches[i].emplace_back(i, indices.at<int>(i, j),  std::sqrt(dists.at<float>(i, j))  );
+        }
+    }
+
+    // matches.resize(query_desc.rows);
+    // for (int qi = 0; qi < query_desc.rows; ++qi) {
+    //     std::vector<cv::DMatch> &dst = matches[qi];
+    //     dst.resize(k);
+    //     for (int ki = 0; ki < k; ++ki) {
+    //         cv::DMatch match;
+    //         match.imgIdx = 0;
+    //         match.queryIdx = qi;
+    //         match.trainIdx = indices.at<int>(qi, ki);
+    //         match.distance = std::sqrt(dists.at<float>(qi, ki));
+    //         dst[ki] = match;
+    //     }
+    // }
+
+
 }
