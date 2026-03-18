318361efa7920337d59c78b23ad270d0ad11de0e
SheLesTT <lovsevdenis3513@gmail.com>
2026-03-18 16:07:58 +0300

    task02

--- src/phg/matching/flann_matcher.cpp
+++ src/phg/matching/flann_matcher.cpp
@@ -6,8 +6,8 @@
 phg::FlannMatcher::FlannMatcher()
 {
     // параметры для приближенного поиска
-//    index_params = flannKdTreeIndexParams(TODO);
-//    search_params = flannKsTreeSearchParams(TODO);
+    index_params = flannKdTreeIndexParams(4);
+    search_params = flannKsTreeSearchParams(32);
 }
 
 void phg::FlannMatcher::train(const cv::Mat &train_desc)
@@ -17,5 +17,16 @@ void phg::FlannMatcher::train(const cv::Mat &train_desc)
 
 void phg::FlannMatcher::knnMatch(const cv::Mat &query_desc, std::vector<std::vector<cv::DMatch>> &matches, int k) const
 {
-    throw std::runtime_error("not implemented yet");
+    cv::Mat indices(query_desc.rows, k, CV_32SC1);
+    cv::Mat dists(query_desc.rows, k, CV_32FC1);
+    flann_index->knnSearch(query_desc, indices, dists,k, *search_params);
+    matches.resize(query_desc.rows);
+    for(int qd_idx = 0; qd_idx< query_desc.rows; qd_idx++){
+       std::vector<cv::DMatch> &d_matches = matches[qd_idx];
+        d_matches.resize(k);
+       for (int i =0; i< k; i++){
+            cv::DMatch match(qd_idx, indices.at<int>(qd_idx, i),0,std::sqrt(dists.at<float>(qd_idx, i)));
+            d_matches[i] = match;
+       }
+    }
 }
