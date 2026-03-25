edffdd6fbf8164299596fc02e14cbc3f27daeb94
SheLesTT <lovsevdenis3513@gmail.com>
2026-03-25 12:40:05 +0300

    task03

--- src/phg/sfm/ematrix.cpp
+++ src/phg/sfm/ematrix.cpp
@@ -18,9 +18,29 @@ namespace {
         copy(Ecv, E);
 
         Eigen::JacobiSVD<Eigen::MatrixXd> svd(E, Eigen::ComputeFullU | Eigen::ComputeFullV);
-        throw std::runtime_error("not implemented yet");
-// TODO
+        Eigen::MatrixXd U = svd.matrixU();
+        Eigen::VectorXd s = svd.singularValues();
+        Eigen::MatrixXd V = svd.matrixV();
+        if (U.determinant() < 0) {
+            U = -U;
+        }
+        if (V.determinant() < 0) {
+            V = -V;
+        }
+        
+        if (s[0] < 1e-10 || s[1] < 1e-10) {
+            std::cout << "ensureSpectralProperty: s: " << s.transpose() << std::endl;
+            throw std::runtime_error("essential matrix is degenerate");
+        }
+        double avg = (s[0] + s[1]) / 2.0;
+        s[0] = avg; 
+        s[1] = avg; 
+        s[2] = 0;
 
+        Eigen::MatrixXd S = Eigen::MatrixXd::Zero(3, 3);
+        S(0, 0) = s[0];
+        S(1, 1) = s[1];
+        E = U * S * V.transpose();
         copy(E, Ecv);
     }
 
@@ -28,12 +48,11 @@ namespace {
 
 cv::Matx33d phg::fmatrix2ematrix(const cv::Matx33d &F, const phg::Calibration &calib0, const phg::Calibration &calib1)
 {
-    throw std::runtime_error("not implemented yet");
-//    matrix3d E = TODO;
-//
-//    ensureSpectralProperty(E);
-//
-//    return E;
+    // convert fundamental matrix to essential matrix using camera calibration
+    // E = K1^T * F * K0
+     cv::Matx33d E = calib1.K().t() * F * calib0.K();
+     ensureSpectralProperty(E);
+     return E;
 }
 
 namespace {
@@ -58,110 +77,122 @@ namespace {
 
         return result;
     }
+    
 
     bool depthTest(const vector2d &m0, const vector2d &m1, const phg::Calibration &calib0, const phg::Calibration &calib1, const matrix34d &P0, const matrix34d &P1)
     {
-        throw std::runtime_error("not implemented yet");
-//        // скомпенсировать калибровки камер
-//        vector3d p0 = TODO;
-//        vector3d p1 = TODO;
-//
-//        vector3d ps[2] = {p0, p1};
-//        matrix34d Ps[2] = {P0, P1};
-//
-//        vector4d X = phg::triangulatePoint(Ps, ps, 2);
-//        if (X[3] != 0) {
-//            X /= X[3];
-//        }
-//
-//        // точка должна иметь положительную глубину для обеих камер
-//        return TODO && TODO;
+    // in front of cameras (positive depths)
+    // positive depth means normalized image point (x, y, 1) should have positive
+    //  dot product with the ray from camera center to the point
+
+       // скомпенсировать калибровки камер
+       vector3d p0 = calib0.unproject(m0);
+       vector3d p1 = calib1.unproject(m1);
+
+       vector3d ps[2] = {p0, p1};
+       matrix34d Ps[2] = {P0, P1};
+
+       vector4d X = phg::triangulatePoint(Ps, ps, 2);
+       if (X[3] != 0) {
+           X /= X[3];
+       }
+
+
+      double depth0 = P0(2, 0) * X[0] + P0(2, 1) * X[1] + P0(2, 2) * X[2] + P0(2, 3);
+        double depth1 = P1(2, 0) * X[0] + P1(2, 1) * X[1] + P1(2, 2) * X[2] + P1(2, 3);
+        return depth0 > 0 && depth1 > 0;
+
     }
 }
 
 // Матрицы камер для фундаментальной матрицы определены с точностью до проективного преобразования
 // То есть, можно исказить трехмерный мир (применив 4-мерную однородную матрицу), и одновременно поменять матрицы P0, P1 так, что проекции в пикселях не изменятся
-// Если мы знаем калибровки камер (матрицы K0, K1 в структуре матриц P0, P1), то можем наложить дополнительные ограничения, в частности, известно, что
-// существенная матрица (Essential matrix = K1t * F * K0) имеет ровно два совпадающих ненулевых сингулярных значения, тогда как для фундаментальной матрицы они могут различаться
+// Если мы знаем калибровки камер (матрицы K0, K1 в структуре матриц P0, P1), то можем наложить дополнительные ограничения, 
+// в частности, известно, что:
+// существенная матрица (Essential matrix = K1t * F * K0) имеет ровно ДВА совпадающих ненулевых сингулярных значения, 
+// тогда как для фундаментальной матрицы они могут различаться
+
 // Это дополнительное ограничение позволяет разложить существенную матрицу с точностью до 4 решений, вместо произвольного проективного преобразования (см. Hartley & Zisserman p.258)
 // Обычно мы можем использовать одну общую калибровку, более менее верную для большого количества реальных камер и с ее помощью выполнить
 // первичное разложение существенной матрицы (а из него, взаимное расположение камер) для последующего уточнения методом нелинейной оптимизации
-void phg::decomposeEMatrix(cv::Matx34d &P0, cv::Matx34d &P1, const cv::Matx33d &Ecv, const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, const Calibration &calib0, const Calibration &calib1)
+void phg::decomposeEMatrix(cv::Matx34d &P0, cv::Matx34d &P1, const cv::Matx33d &Ecv, 
+    const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, const Calibration &calib0, const Calibration &calib1)
 {
-    throw std::runtime_error("not implemented yet");
-//    if (m0.size() != m1.size()) {
-//        throw std::runtime_error("decomposeEMatrix : m0.size() != m1.size()");
-//    }
-//
-//    using mat = Eigen::MatrixXd;
-//    using vec = Eigen::VectorXd;
-//
-//    mat E;
-//    copy(Ecv, E);
-//
-//    // (см. Hartley & Zisserman p.258)
-//
-//    Eigen::JacobiSVD<mat> svd(E, Eigen::ComputeFullU | Eigen::ComputeFullV);
-//
-//    mat U = svd.matrixU();
-//    vec s = svd.singularValues();
-//    mat V = svd.matrixV();
-//
-//    // U, V must be rotation matrices, not just orthogonal
-//    if (U.determinant() < 0) U = -U;
-//    if (V.determinant() < 0) V = -V;
-//
-//    std::cout << "U:\n" << U << std::endl;
-//    std::cout << "s:\n" << s << std::endl;
-//    std::cout << "V:\n" << V << std::endl;
-//
-//
-//    mat R0 = TODO;
-//    mat R1 = TODO;
-//
-//    std::cout << "R0:\n" << R0 << std::endl;
-//    std::cout << "R1:\n" << R1 << std::endl;
-//
-//    vec t0 = TODO;
-//    vec t1 = TODO;
-//
-//    std::cout << "t0:\n" << t0 << std::endl;
-//
-//    P0 = matrix34d::eye();
-//
-//    // 4 possible solutions
-//    matrix34d P10 = composeP(R0, t0);
-//    matrix34d P11 = composeP(R0, t1);
-//    matrix34d P12 = composeP(R1, t0);
-//    matrix34d P13 = composeP(R1, t1);
-//    matrix34d P1s[4] = {P10, P11, P12, P13};
-//
-//    // need to select best of 4 solutions: 3d points should be in front of cameras (positive depths)
-//    int best_count = 0;
-//    int best_idx = -1;
-//    for (int i = 0; i < 4; ++i) {
-//        int count = 0;
-//        for (int j = 0; j < (int) m0.size(); ++j) {
-//            if (depthTest(m0[j], m1[j], calib0, calib1, P0, P1s[i])) {
-//                ++count;
-//            }
-//        }
-//        std::cout << "decomposeEMatrix: count: " << count << std::endl;
-//        if (count > best_count) {
-//            best_count = count;
-//            best_idx = i;
-//        }
-//    }
-//
-//    if (best_count == 0) {
-//        throw std::runtime_error("decomposeEMatrix : can't decompose ematrix");
-//    }
-//
-//    P1 = P1s[best_idx];
-//
-//    std::cout << "best idx: " << best_idx << std::endl;
-//    std::cout << "P0: \n" << P0 << std::endl;
-//    std::cout << "P1: \n" << P1 << std::endl;
+   if (m0.size() != m1.size()) {
+       throw std::runtime_error("decomposeEMatrix : m0.size() != m1.size()");
+   }
+
+   using mat = Eigen::MatrixXd;
+   using vec = Eigen::VectorXd;
+
+   mat E;
+   copy(Ecv, E);
+
+   // (см. Hartley & Zisserman p.258)
+
+   Eigen::JacobiSVD<mat> svd(E, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+   mat U = svd.matrixU();
+   vec s = svd.singularValues();
+   mat V = svd.matrixV();
+
+   // U, V must be rotation matrices, not just orthogonal
+   if (U.determinant() < 0) U = -U;
+   if (V.determinant() < 0) V = -V;
+
+   std::cout << "U:\n" << U << std::endl;
+   std::cout << "s:\n" << s << std::endl;
+   std::cout << "V:\n" << V << std::endl;
+   // alternative SVD is given by
+// E = (U diag(R2×2, 1)) diag(1, 1, 0)(diag(RT 2×2, 1))VT for any 2 × 2 rotation matrix R.
+
+// then
+   mat R0 = U * mat{{0, -1, 0}, {1, 0, 0}, {0, 0, 1}} * V.transpose();
+   mat R1 = U * mat{{0, 1, 0}, {-1, 0, 0}, {0, 0, 1}} * V.transpose();
+
+   std::cout << "R0:\n" << R0 << std::endl;
+   std::cout << "R1:\n" << R1 << std::endl;
+
+   vec t0 = U.col(2);
+   vec t1 = -U.col(2);
+
+   std::cout << "t0:\n" << t0 << std::endl;
+
+   P0 = matrix34d::eye();
+
+   // we found 4 possible solutions
+   matrix34d P10 = composeP(R0, t0);
+   matrix34d P11 = composeP(R0, t1);
+   matrix34d P12 = composeP(R1, t0);
+   matrix34d P13 = composeP(R1, t1);
+   matrix34d P1s[4] = {P10, P11, P12, P13};
+
+   // need to select best of 4 solutions: 3d points should be in front of cameras (positive depths)
+   int best_count = 0;
+   int best_idx = -1;
+   for (int i = 0; i < 4; ++i) {
+       int count = 0;
+       for (int j = 0; j < (int) m0.size(); ++j) {
+           if (depthTest(m0[j], m1[j], calib0, calib1, P0, P1s[i])) {
+               ++count;
+           }
+       }
+       std::cout << "decomposeEMatrix: count: " << count << std::endl;
+       if (count > best_count) {
+           best_count = count;
+           best_idx = i;
+       }
+   }
+
+   if (best_count == 0) {
+       throw std::runtime_error("decomposeEMatrix : can't decompose ematrix");
+   }
+
+   P1 = P1s[best_idx];
+
+   std::cout << "best idx: " << best_idx << std::endl;
+   std::cout << "P0: \n" << P0 << std::endl;
+   std::cout << "P1: \n" << P1 << std::endl;
 }
 
 void phg::decomposeUndistortedPMatrix(cv::Matx33d &R, cv::Vec3d &O, const cv::Matx34d &P)
--- src/phg/sfm/triangulation.cpp
+++ src/phg/sfm/triangulation.cpp
@@ -1,7 +1,7 @@
 #include "triangulation.h"
 
 #include "defines.h"
-
+#include "iostream"
 #include <Eigen/SVD>
 
 // По положениям камер и ключевых точкам определяем точку в трехмерном пространстве
@@ -10,7 +10,25 @@
 // (см. Hartley & Zisserman p.312)
 cv::Vec4d phg::triangulatePoint(const cv::Matx34d *Ps, const cv::Vec3d *ms, int count)
 {
-    // составление однородной системы + SVD
-    // без подвохов
-    throw std::runtime_error("not implemented yet");
+    Eigen::MatrixXd A(count*2, 4);
+
+    for (int i = 0; i < count; ++i) {
+        const cv::Matx34d &P = Ps[i];
+        const cv::Vec3d &m = ms[i];
+
+        A.row(i * 2) << m[0] * P(2, 0) - P(0, 0),
+                    m[0] * P(2, 1) - P(0, 1),
+                    m[0] * P(2, 2) - P(0, 2),
+                    m[0] * P(2, 3) - P(0, 3);
+
+
+        A.row(i * 2 + 1) << m[1] * P(2, 0) - P(1, 0),
+                        m[1] * P(2, 1) - P(1, 1),
+                        m[1] * P(2, 2) - P(1, 2),
+                        m[1] * P(2, 3) - P(1, 3);
+    }
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullV);
+    Eigen::VectorXd X = svd.matrixV().col(3); 
+    return cv::Vec4d(X(0), X(1), X(2), X(3));
 }
