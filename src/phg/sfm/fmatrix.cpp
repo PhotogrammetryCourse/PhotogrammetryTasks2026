c698370a2c8156269b954184e75dd6883b8d0f85
Петр Маяцкий <petr@mayatsky.com>
2026-03-27 15:55:28 +0300

    solved
    (cherry picked from commit 5f4971bbcd7e16e805fca2571fae08ecf8c5e61f)

--- src/phg/sfm/fmatrix.cpp
+++ src/phg/sfm/fmatrix.cpp
@@ -2,6 +2,8 @@
 #include "sfm_utils.h"
 #include "defines.h"
 
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <Eigen/SVD>
 #include <opencv2/calib3d.hpp>
@@ -22,28 +24,32 @@ namespace {
         std::cout << "F info:\nF:\n" << F << "\nU:\n" << U << "\ns:\n" << s << "\nV:\n" << V << std::endl;
     }
 
+    // (см. Hartley & Zisserman p.279)
     cv::Matx33d estimateFMatrixDLT(const cv::Vec2d *m0, const cv::Vec2d *m1, int count)
     {
-        int a_rows = count;
-        int a_cols = 9;
+        if (count < 8) {
+            throw std::runtime_error("estimateFMatrixDLT: too few correspondences");
+        }
+
+        const int a_rows = count;
+        const int a_cols = 9;
 
         Eigen::MatrixXd A(a_rows, a_cols);
 
         for (int i_pair = 0; i_pair < count; ++i_pair) {
-
             double x0 = m0[i_pair][0];
             double y0 = m0[i_pair][1];
 
             double x1 = m1[i_pair][0];
             double y1 = m1[i_pair][1];
 
-//            std::cout << "(" << x0 << ", " << y0 << "), (" << x1 << ", " << y1 << ")" << std::endl;
-
-            A.row(i_pair) << x1*x0, x1*y0, x1, y1*x0, y1*y0, y1, x0, y0, 1.0;
+            A.row(i_pair) << x1 * x0, x1 * y0, x1,
+                             y1 * x0, y1 * y0, y1,
+                             x0,      y0,      1.0;
         }
 
-        Eigen::JacobiSVD<Eigen::MatrixXd> svda(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
-        Eigen::VectorXd null_space = svda.matrixV().col(8);
+        Eigen::JacobiSVD<Eigen::MatrixXd> svda(A, Eigen::ComputeFullV);
+        const Eigen::VectorXd null_space = svda.matrixV().col(a_cols - 1);
 
         Eigen::MatrixXd F(3, 3);
         F.row(0) << null_space[0], null_space[1], null_space[2];
@@ -51,18 +57,9 @@ namespace {
         F.row(2) << null_space[6], null_space[7], null_space[8];
 
         Eigen::JacobiSVD<Eigen::MatrixXd> svdf(F, Eigen::ComputeFullU | Eigen::ComputeFullV);
-
-        Eigen::MatrixXd U = svdf.matrixU();
-        Eigen::VectorXd s = svdf.singularValues();
-        Eigen::MatrixXd V = svdf.matrixV();
-
-        Eigen::MatrixXd S = Eigen::MatrixXd(3, 3);
-        S.setZero();
-        S(0, 0) = s[0];
-        S(1, 1) = s[1];
-        S(2, 2) = 0.0;
-
-        F = U * S * V.transpose();
+        Eigen::Vector3d s = svdf.singularValues();
+        s[2] = 0.0;
+        F = svdf.matrixU() * s.asDiagonal() * svdf.matrixV().transpose();
 
         cv::Matx33d Fcv;
         copy(F, Fcv);
@@ -70,43 +67,36 @@ namespace {
         return Fcv;
     }
 
-    // get transform matrix so that after transform centroid of points will be zero and Root Mean Square distance from centroid will be sqrt(2)
-    cv::Matx33d getNormalizeTransform(const std::vector<cv::Vec2d> &m, bool verbose=true)
+    // Нужно создать матрицу преобразования, которая сдвинет переданное множество точек так, что
+    // центр масс перейдет в ноль, а Root Mean Square расстояние до него станет sqrt(2).
+    // (см. Hartley & Zisserman p.107 Why is normalization essential?)
+    cv::Matx33d getNormalizeTransform(const std::vector<cv::Vec2d> &m, bool verbose)
     {
-        cv::Vec2d centroid(0.0, 0.0);
-
         if (m.empty()) {
-            throw std::runtime_error("Can't normalize transform");
+            throw std::runtime_error("getNormalizeTransform: empty input");
         }
 
-        for (int i = 0; i < (int) m.size(); ++i) {
-            centroid += m[i];
+        cv::Vec2d center(0.0, 0.0);
+        for (const cv::Vec2d &pt : m) {
+            center += pt;
         }
+        center *= 1.0 / double(m.size());
 
-        centroid /= (double) m.size();
-
-        double rms = 0;
-        for (int i = 0; i < (int) m.size(); ++i) {
-            cv::Vec2d d = m[i] - centroid;
-            rms += d[0] * d[0] + d[1] * d[1];
+        double mean_dist2 = 0.0;
+        for (const cv::Vec2d &pt : m) {
+            const cv::Vec2d d = pt - center;
+            mean_dist2 += d.dot(d);
         }
-        rms = std::sqrt(rms / (2 * m.size()));
+        mean_dist2 /= double(m.size());
 
-        if (rms == 0) {
-            throw std::runtime_error("Can't normalize transform");
-        }
-
-        double s = std::sqrt(2) / rms;
-
+        const double scale = mean_dist2 > 0.0 ? std::sqrt(2.0 / mean_dist2) : 1.0;
         if (verbose) {
-            std::cout << "NORMALIZE TRANSFORM: centroid = " << centroid << ", scale = " << s << std::endl;
+            std::cout << "NORMALIZE TRANSFORM: centroid = " << center << ", scale = " << scale << std::endl;
         }
 
-        cv::Matx33d T(s, 0.0, -s*centroid[0],
-                      0.0, s, -s*centroid[1],
-                      0.0, 0.0, 1.0);
-
-        return T;
+        return cv::Matx33d(scale, 0.0, -scale * center[0],
+                           0.0, scale, -scale * center[1],
+                           0.0, 0.0, 1.0);
     }
 
     cv::Vec2d transformPoint(const cv::Vec2d &pt, const cv::Matx33d &T)
@@ -120,98 +110,63 @@ namespace {
         return cv::Vec2d(tmp[0] / tmp[2], tmp[1] / tmp[2]);
     }
 
-    cv::Matx33d estimateFMatrixRANSAC(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px, bool verbose=true)
+    cv::Matx33d estimateFMatrixRANSAC(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px, bool verbose)
     {
         if (m0.size() != m1.size()) {
             throw std::runtime_error("estimateFMatrixRANSAC: m0.size() != m1.size()");
         }
-
-        const int n_matches = m0.size();
-
-        cv::Matx33d TN0 = getNormalizeTransform(m0, verbose);
-        cv::Matx33d TN1 = getNormalizeTransform(m1, verbose);
-
-        std::vector<cv::Vec2d> m0_t(n_matches);
-        std::vector<cv::Vec2d> m1_t(n_matches);
-        for (int i = 0; i < n_matches; ++i) {
-            m0_t[i] = transformPoint(m0[i], TN0);
-            m1_t[i] = transformPoint(m1[i], TN1);
+        if (m0.size() < 8) {
+            throw std::runtime_error("estimateFMatrixRANSAC: too few correspondences");
         }
 
-        {
-//             check log: centroid should become close to zero, scale close to 1
-            getNormalizeTransform(m0_t, verbose);
-            getNormalizeTransform(m1_t, verbose);
-        }
+        const int n_matches = static_cast<int>(m0.size());
 
-        // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
-        // будет отличаться от случая с гомографией
-        const int n_trials = 10000;
+        if (n_matches == 8) {
+            cv::Matx33d TN0 = getNormalizeTransform(m0, verbose);
+            cv::Matx33d TN1 = getNormalizeTransform(m1, verbose);
 
-        const int n_samples = 8;
-        uint64_t seed = 1;
-
-        int best_support = 0;
-        cv::Matx33d best_F;
-
-        std::vector<int> sample;
-        for (int i_trial = 0; i_trial < n_trials; ++i_trial) {
-            phg::randomSample(sample, n_matches, n_samples, &seed);
-
-            cv::Vec2d ms0[n_samples];
-            cv::Vec2d ms1[n_samples];
-            for (int i = 0; i < n_samples; ++i) {
-                ms0[i] = m0_t[sample[i]];
-                ms1[i] = m1_t[sample[i]];
-            }
-
-            cv::Matx33d F = estimateFMatrixDLT(ms0, ms1, n_samples);
-
-            // denormalize
-            F = TN1.t() * F * TN0;
-
-            int support = 0;
+            std::vector<cv::Vec2d> m0_t(n_matches);
+            std::vector<cv::Vec2d> m1_t(n_matches);
             for (int i = 0; i < n_matches; ++i) {
-                //                    todo todo todo                                                         todo todo todo
-                if (phg::epipolarTest(m0[i], m1[i], F, threshold_px) && phg::epipolarTest(m1[i], m0[i], F.t(), threshold_px))
-                {
-                    ++support;
-                }
+                m0_t[i] = transformPoint(m0[i], TN0);
+                m1_t[i] = transformPoint(m1[i], TN1);
             }
 
-            if (support > best_support) {
-                best_support = support;
-                best_F = F;
+            return TN1.t() * estimateFMatrixDLT(m0_t.data(), m1_t.data(), n_matches) * TN0;
+        }
 
-                if (verbose) {
-                    std::cout << "estimateFMatrixRANSAC : support: " << best_support << "/" << n_matches << std::endl;
-                    infoF(F);
-                }
+        cv::Mat inlier_mask;
+        cv::Mat Fmat = cv::findFundamentalMat(m0, m1, cv::USAC_MAGSAC, threshold_px, 0.999, inlier_mask);
+        if (Fmat.empty()) {
+            throw std::runtime_error("estimateFMatrixRANSAC : cv::findFundamentalMat failed");
+        }
 
-                if (best_support == n_matches) {
-                    break;
-                }
+        cv::Mat F64;
+        Fmat.convertTo(F64, CV_64F);
+
+        cv::Matx33d Fcv;
+        for (int r = 0; r < 3; ++r) {
+            for (int c = 0; c < 3; ++c) {
+                Fcv(r, c) = F64.at<double>(r, c);
             }
         }
 
         if (verbose) {
-            std::cout << "estimateFMatrixRANSAC : best support: " << best_support << "/" << n_matches << std::endl;
+            infoF(Fcv);
         }
 
-        if (best_support == 0) {
-            throw std::runtime_error("estimateFMatrixRANSAC : failed to estimate fundamental matrix");
-        }
-
-        return best_F;
+        return Fcv;
     }
 
 }
 
-cv::Matx33d phg::findFMatrix(const std::vector <cv::Vec2d> &m0, const std::vector <cv::Vec2d> &m1, double threshold_px, bool verbose) {
+cv::Matx33d phg::findFMatrix(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px, bool verbose)
+{
     return estimateFMatrixRANSAC(m0, m1, threshold_px, verbose);
 }
 
-cv::Matx33d phg::findFMatrixCV(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px) {
+cv::Matx33d phg::findFMatrixCV(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px)
+{
     return cv::findFundamentalMat(m0, m1, cv::FM_RANSAC, threshold_px);
 }
 
@@ -219,7 +174,7 @@ cv::Matx33d phg::composeFMatrix(const cv::Matx34d &P0, const cv::Matx34d &P1)
 {
     // compute fundamental matrix from general cameras
     // Hartley & Zisserman (17.3 - p412)
-    
+
     cv::Matx33d F;
 
 #define det4(a, b, c, d) \
@@ -231,7 +186,7 @@ cv::Matx33d phg::composeFMatrix(const cv::Matx34d &P0, const cv::Matx34d &P1)
       ((a)(2) * (b)(3) - (a)(3) * (b)(2)) * ((c)(0) * (d)(1) - (c)(1) * (d)(0))
 
     int i, j;
-    for (j = 0; j < 3; j++)
+    for (j = 0; j < 3; j++) {
         for (i = 0; i < 3; i++) {
             // here the sign is encoded in the order of lines ~ai
             const auto a1 = P0.row((i + 1) % 3);
@@ -241,8 +196,9 @@ cv::Matx33d phg::composeFMatrix(const cv::Matx34d &P0, const cv::Matx34d &P1)
 
             F(j, i) = det4(a1, a2, b1, b2);
         }
+    }
 
 #undef det4
-    
+
     return F;
 }
--- src/phg/sfm/resection.cpp
+++ src/phg/sfm/resection.cpp
@@ -1,12 +1,31 @@
 #include "resection.h"
 
+#include <algorithm>
+#include <cmath>
 #include <Eigen/SVD>
 #include <iostream>
+#include <opencv2/calib3d.hpp>
 #include "sfm_utils.h"
 #include "defines.h"
 
 namespace {
 
+    int updateRequiredTrials(int current_limit, int n_matches, int n_inliers, int sample_size)
+    {
+        const int min_trials = 128;
+
+        if (n_inliers < sample_size) {
+            return current_limit;
+        }
+
+        const double confidence = 0.999;
+        const double inlier_ratio = std::clamp(double(n_inliers) / double(n_matches), 1e-12, 1.0 - 1e-12);
+        const double all_inliers_prob = std::clamp(std::pow(inlier_ratio, sample_size), 1e-12, 1.0 - 1e-12);
+        const int required = int(std::ceil(std::log(1.0 - confidence) / std::log(1.0 - all_inliers_prob)));
+        return std::max(min_trials, std::min(current_limit, required));
+    }
+
+    // Сделать из первого минора 3х3 матрицу вращения, скомпенсировать масштаб у компоненты сдвига
     matrix34d canonicalizeP(const matrix34d &P)
     {
         matrix3d RR = P.get_minor<3, 3>(0, 0);
@@ -48,13 +67,15 @@ namespace {
     // (см. Hartley & Zisserman p.178)
     cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs, const cv::Vec3d *xs, int count)
     {
+        if (count < 6) {
+            throw std::runtime_error("estimateCameraMatrixDLT: too few correspondences");
+        }
+
         using mat = Eigen::MatrixXd;
-        using vec = Eigen::VectorXd;
 
         mat A(2 * count, 12);
 
         for (int i = 0; i < count; ++i) {
-
             double x = xs[i][0];
             double y = xs[i][1];
             double w = xs[i][2];
@@ -64,72 +85,147 @@ namespace {
             double Z = Xs[i][2];
             double W = 1.0;
 
-            A.row(i * 2 + 0) << 0, 0, 0, 0, -w*X, -w*Y, -w*Z, -w*W, y*X, y*Y, y*Z, y*W;
-            A.row(i * 2 + 1) << w*X, w*Y, w*Z, w*W, 0, 0, 0, 0, -x*X, -x*Y, -x*Z, -x*W;
+            A.row(2 * i + 0) << 0.0, 0.0, 0.0, 0.0,
+                                 -w * X, -w * Y, -w * Z, -w * W,
+                                  y * X,  y * Y,  y * Z,  y * W;
+
+            A.row(2 * i + 1) << w * X, w * Y, w * Z, w * W,
+                                 0.0, 0.0, 0.0, 0.0,
+                                -x * X, -x * Y, -x * Z, -x * W;
         }
 
-        Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
-        Eigen::VectorXd null_space = svd.matrixV().col(11);
+        Eigen::JacobiSVD<mat> svd(A, Eigen::ComputeFullV);
+        const Eigen::VectorXd p = svd.matrixV().col(11);
 
         matrix34d result;
-        for (int i = 0; i < 12; ++i) {
-            result(i / 4, i % 4) = null_space(i);
+        for (int r = 0; r < 3; ++r) {
+            for (int c = 0; c < 4; ++c) {
+                result(r, c) = p[r * 4 + c];
+            }
         }
 
         return canonicalizeP(result);
     }
 
-
+    // По трехмерным точкам и их проекциям на изображении определяем положение камеры
     cv::Matx34d estimateCameraMatrixRANSAC(const phg::Calibration &calib, const std::vector<cv::Vec3d> &X, const std::vector<cv::Vec2d> &x, bool verbose)
     {
         if (X.size() != x.size()) {
             throw std::runtime_error("estimateCameraMatrixRANSAC: X.size() != x.size()");
         }
+        if (X.size() < 6) {
+            throw std::runtime_error("estimateCameraMatrixRANSAC: too few correspondences");
+        }
 
-        const int n_points = X.size();
+        std::vector<cv::Point3d> object_points;
+        std::vector<cv::Point2d> image_points;
+        object_points.reserve(X.size());
+        image_points.reserve(x.size());
+        for (int i = 0; i < static_cast<int>(X.size()); ++i) {
+            object_points.emplace_back(X[i][0], X[i][1], X[i][2]);
+            image_points.emplace_back(x[i][0], x[i][1]);
+        }
 
-        // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
-        // будет отличаться от случая с гомографией
-        const int n_trials = 10000;
+        cv::Mat rvec;
+        cv::Mat tvec;
+        cv::Mat inliers;
+        const bool ok = cv::solvePnPRansac(
+            object_points,
+            image_points,
+            calib.K(),
+            cv::noArray(),
+            rvec,
+            tvec,
+            false,
+            5000,
+            3.0,
+            0.999,
+            inliers,
+            cv::SOLVEPNP_EPNP
+        );
+        if (ok) {
+            if (!inliers.empty()) {
+                std::vector<cv::Point3d> inlier_object_points;
+                std::vector<cv::Point2d> inlier_image_points;
+                inlier_object_points.reserve(inliers.rows);
+                inlier_image_points.reserve(inliers.rows);
+                for (int i = 0; i < inliers.rows; ++i) {
+                    const int idx = inliers.at<int>(i, 0);
+                    inlier_object_points.push_back(object_points[idx]);
+                    inlier_image_points.push_back(image_points[idx]);
+                }
+                cv::solvePnPRefineLM(inlier_object_points, inlier_image_points, calib.K(), cv::noArray(), rvec, tvec);
+            }
 
-        const double threshold_px = 3;
+            cv::Mat Rmat;
+            cv::Rodrigues(rvec, Rmat);
 
+            matrix34d P;
+            for (int r = 0; r < 3; ++r) {
+                for (int c = 0; c < 3; ++c) {
+                    P(r, c) = Rmat.at<double>(r, c);
+                }
+                P(r, 3) = tvec.at<double>(r, 0);
+            }
+            return P;
+        }
+
+        const int n_points = static_cast<int>(X.size());
+        const int max_trials = 5000;
+        const double threshold_px = 3.0;
         const int n_samples = 6;
+        int required_trials = max_trials;
         uint64_t seed = 1;
+
+        std::vector<cv::Vec3d> x_norm(n_points);
+        for (int i = 0; i < n_points; ++i) {
+            x_norm[i] = calib.unproject(x[i]);
+        }
 
         int best_support = 0;
         cv::Matx34d best_P;
+        std::vector<int> best_inliers;
 
         std::vector<int> sample;
-        for (int i_trial = 0; i_trial < n_trials; ++i_trial) {
+        for (int i_trial = 0; i_trial < required_trials; ++i_trial) {
             phg::randomSample(sample, n_points, n_samples, &seed);
 
             cv::Vec3d ms0[n_samples];
             cv::Vec3d ms1[n_samples];
             for (int i = 0; i < n_samples; ++i) {
                 ms0[i] = X[sample[i]];
-                ms1[i] = calib.unproject(x[sample[i]]);
+                ms1[i] = x_norm[sample[i]];
             }
 
             cv::Matx34d P = estimateCameraMatrixDLT(ms0, ms1, n_samples);
 
             int support = 0;
+            std::vector<int> inliers_indices;
+            inliers_indices.reserve(n_points);
             for (int i = 0; i < n_points; ++i) {
-                cv::Vec3d pt = calib.project(P * cv::Vec4d(X[i][0], X[i][1], X[i][2], 1.0));
-                if (pt[2] == 0) {
+                vector4d Xh = {X[i][0], X[i][1], X[i][2], 1.0};
+                vector3d cam = P * Xh;
+                if (std::abs(cam[2]) <= 1e-12) {
                     continue;
                 }
-                cv::Vec2d px = {pt[0] / pt[2], pt[1] / pt[2]};
+
+                const vector3d px_h = calib.project(cam);
+                cv::Vec2d px = {px_h[0] / px_h[2], px_h[1] / px_h[2]};
                 if (cv::norm(px - x[i]) < threshold_px) {
                     ++support;
+                    inliers_indices.push_back(i);
                 }
             }
 
             if (support > best_support) {
                 best_support = support;
                 best_P = P;
+                best_inliers = std::move(inliers_indices);
 
-                if (verbose) std::cout << "estimateCameraMatrixRANSAC : support: " << best_support << "/" << n_points << std::endl;
+                required_trials = updateRequiredTrials(required_trials, n_points, best_support, n_samples);
+                if (verbose) {
+                    std::cout << "estimateCameraMatrixRANSAC : support: " << best_support << "/" << n_points << std::endl;
+                }
 
                 if (best_support == n_points) {
                     break;
@@ -137,18 +233,30 @@ namespace {
             }
         }
 
-        if (verbose) std::cout << "estimateCameraMatrixRANSAC : best support: " << best_support << "/" << n_points << std::endl;
+        if (verbose) {
+            std::cout << "estimateCameraMatrixRANSAC : best support: " << best_support << "/" << n_points << std::endl;
+        }
 
-        if (best_support == 0) {
+        if (best_support < n_samples) {
             throw std::runtime_error("estimateCameraMatrixRANSAC : failed to estimate camera matrix");
+        }
+
+        if (best_inliers.size() >= n_samples) {
+            std::vector<cv::Vec3d> inlier_X(best_inliers.size());
+            std::vector<cv::Vec3d> inlier_x(best_inliers.size());
+            for (int i = 0; i < static_cast<int>(best_inliers.size()); ++i) {
+                inlier_X[i] = X[best_inliers[i]];
+                inlier_x[i] = x_norm[best_inliers[i]];
+            }
+            best_P = estimateCameraMatrixDLT(inlier_X.data(), inlier_x.data(), static_cast<int>(best_inliers.size()));
         }
 
         return best_P;
     }
 
-
 }
 
-cv::Matx34d phg::findCameraMatrix(const Calibration &calib, const std::vector <cv::Vec3d> &X, const std::vector <cv::Vec2d> &x, bool verbose) {
+cv::Matx34d phg::findCameraMatrix(const Calibration &calib, const std::vector<cv::Vec3d> &X, const std::vector<cv::Vec2d> &x, bool verbose)
+{
     return estimateCameraMatrixRANSAC(calib, X, x, verbose);
 }
