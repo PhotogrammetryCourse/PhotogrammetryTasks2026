73942479e21c7a8c74e3581a9808de670c83dcf7
XIII-git <misha120206@gmail.com>
2026-03-29 22:45:21 +0300

    try use different epipolar test

--- src/phg/sfm/sfm_utils.cpp
+++ src/phg/sfm/sfm_utils.cpp
@@ -41,15 +41,37 @@ void phg::randomSample(std::vector<int> &dst, int max_id, int sample_size, uint6
 // проверяет, что расстояние от точки до линии меньше порога
 bool phg::epipolarTest(const cv::Vec2d &pt0, const cv::Vec2d &pt1, const cv::Matx33d &F, double t)
 {
-    cv::Vec3d pt0_e{pt0[0], pt0[1], 1.0};
 
-    cv::Vec3d l1 = F * pt0_e;
+    cv::Vec3d p0_e{pt0[0], pt0[1], 1.0};
+    cv::Vec3d p1_e{pt1[0], pt1[1], 1.0};
 
-    double dist = std::abs(
-        l1[0] * pt1[0] +
-        l1[1] * pt1[1] +
-        l1[2]
-    ) / std::sqrt(l1[0] * l1[0] + l1[1] * l1[1]);
+    cv::Vec3d Fp0 = F * p0_e;
+    cv::Vec3d Ftp1 = F.t() * p1_e;
 
-    return dist < t;
+    double p1tFp0 = p1_e.dot(Fp0);
+
+    double d =
+        Fp0[0] * Fp0[0] + Fp0[1] * Fp0[1] +
+        Ftp1[0] * Ftp1[0] + Ftp1[1] * Ftp1[1];
+
+    double d2;
+    if (d < 1e-12) {
+        d2 =  std::numeric_limits<double>::infinity();
+    } else {
+        d2 = (p1tFp0 * p1tFp0) / d;
+    }
+
+    return d2 < t * t;
+
+    // cv::Vec3d pt0_e{pt0[0], pt0[1], 1.0};
+    //
+    // cv::Vec3d l1 = F * pt0_e;
+    //
+    // double dist = std::abs(
+    //     l1[0] * pt1[0] +
+    //     l1[1] * pt1[1] +
+    //     l1[2]
+    // ) / std::sqrt(l1[0] * l1[0] + l1[1] * l1[1]);
+    //
+    // return dist < t;
 }
