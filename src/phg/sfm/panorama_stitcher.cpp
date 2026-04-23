2d8163e750cc079d0ce5fb4abd63e0cba6cb159c
Pavel Ralnikov <ralnikovp@gmail.com>
2026-04-23 12:23:41 +0300

    Task02 done

--- src/phg/sfm/panorama_stitcher.cpp
+++ src/phg/sfm/panorama_stitcher.cpp
@@ -1,8 +1,8 @@
 #include "panorama_stitcher.h"
 #include "homography.h"
 
-#include <libutils/bbox2.h>
 #include <iostream>
+#include <libutils/bbox2.h>
 
 /*
  * imgs - список картинок
@@ -10,9 +10,7 @@
  *          этот список образует дерево, корень дерева (картинка, которая ни к кому не приклеивается, приклеиваются только к ней), в данном массиве имеет значение -1
  * homography_builder - функтор, возвращающий гомографию по паре картинок
  * */
-cv::Mat phg::stitchPanorama(const std::vector<cv::Mat> &imgs,
-                            const std::vector<int> &parent,
-                            std::function<cv::Mat(const cv::Mat &, const cv::Mat &)> &homography_builder)
+cv::Mat phg::stitchPanorama(const std::vector<cv::Mat>& imgs, const std::vector<int>& parent, std::function<cv::Mat(const cv::Mat&, const cv::Mat&)>& homography_builder)
 {
     const int n_images = imgs.size();
 
@@ -23,7 +21,30 @@ cv::Mat phg::stitchPanorama(const std::vector<cv::Mat> &imgs,
     {
         // здесь надо посчитать вектор Hs
         // при этом можно обойтись n_images - 1 вызовами функтора homography_builder
-        throw std::runtime_error("not implemented yet");
+        int root = -1;
+        std::vector<std::vector<int>> children(n_images);
+        for (int i = 0; i < n_images; i++) {
+            if (parent[i] == -1) {
+                Hs[i] = cv::Mat::eye(3, 3, CV_64F);
+                root = i;
+            } else {
+                children[parent[i]].emplace_back(i);
+            }
+        }
+
+        if (root == -1) {
+            throw std::runtime_error("Could not find root in given panorama tree");
+        }
+        std::vector<int> ready = { root };
+
+        while (!ready.empty()) {
+            int par = ready.back();
+            ready.pop_back();
+            for (int child : children[par]) {
+                Hs[child] = homography_builder(imgs[child], imgs[par]) * Hs[par];
+                ready.emplace_back(child);
+            }
+        }
     }
 
     bbox2<double, cv::Point2d> bbox;
@@ -46,22 +67,22 @@ cv::Mat phg::stitchPanorama(const std::vector<cv::Mat> &imgs,
     // из-за растяжения пикселей при использовании прямой матрицы гомографии после отображения между пикселями остается пустое пространство
     // лучше использовать обратную и для каждого пикселя на итоговвой картинке проверять, с какой картинки он может получить цвет
     // тогда в некоторых пикселях цвет будет дублироваться, но изображение будет непрерывным
-//        for (int i = 0; i < n_images; ++i) {
-//            for (int y = 0; y < imgs[i].rows; ++y) {
-//                for (int x = 0; x < imgs[i].cols; ++x) {
-//                    cv::Vec3b color = imgs[i].at<cv::Vec3b>(y, x);
-//
-//                    cv::Point2d pt_dst = applyH(cv::Point2d(x, y), Hs[i]) - bbox.min();
-//                    int y_dst = std::max(0, std::min((int) std::round(pt_dst.y), result_height - 1));
-//                    int x_dst = std::max(0, std::min((int) std::round(pt_dst.x), result_width - 1));
-//
-//                    result.at<cv::Vec3b>(y_dst, x_dst) = color;
-//                }
-//            }
-//        }
+    //        for (int i = 0; i < n_images; ++i) {
+    //            for (int y = 0; y < imgs[i].rows; ++y) {
+    //                for (int x = 0; x < imgs[i].cols; ++x) {
+    //                    cv::Vec3b color = imgs[i].at<cv::Vec3b>(y, x);
+    //
+    //                    cv::Point2d pt_dst = applyH(cv::Point2d(x, y), Hs[i]) - bbox.min();
+    //                    int y_dst = std::max(0, std::min((int) std::round(pt_dst.y), result_height - 1));
+    //                    int x_dst = std::max(0, std::min((int) std::round(pt_dst.x), result_width - 1));
+    //
+    //                    result.at<cv::Vec3b>(y_dst, x_dst) = color;
+    //                }
+    //            }
+    //        }
 
     std::vector<cv::Mat> Hs_inv;
-    std::transform(Hs.begin(), Hs.end(), std::back_inserter(Hs_inv), [&](const cv::Mat &H){ return H.inv(); });
+    std::transform(Hs.begin(), Hs.end(), std::back_inserter(Hs_inv), [&](const cv::Mat& H) { return H.inv(); });
 
 #pragma omp parallel for
     for (int y = 0; y < result_height; ++y) {
@@ -82,7 +103,6 @@ cv::Mat phg::stitchPanorama(const std::vector<cv::Mat> &imgs,
                     break;
                 }
             }
-
         }
     }
 
