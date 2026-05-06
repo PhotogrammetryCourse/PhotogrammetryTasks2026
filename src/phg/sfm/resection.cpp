2eb59dcafbff55555b6d789d34e542d41bf99d87
Timur <timursyrma@example.org>
2026-05-06 14:49:12 +0300

    Hartley-Zisserman point

--- src/phg/sfm/resection.cpp
+++ src/phg/sfm/resection.cpp
@@ -47,23 +47,71 @@ namespace {
         return result;
     }
 
-    // (см. Hartley & Zisserman p.178)
-    cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs, const cv::Vec3d *xs, int count)
+    cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs_in, const cv::Vec3d *xs_in, int count)
     {
         using mat = Eigen::MatrixXd;
+
+        cv::Vec3d c3(0, 0, 0);
+        for (int i = 0; i < count; ++i) c3 += Xs_in[i];
+        c3 *= (1.0 / count);
+        double rms3 = 0;
+        for (int i = 0; i < count; ++i) {
+            cv::Vec3d d = Xs_in[i] - c3;
+            rms3 += d.dot(d);
+        }
+        rms3 = std::sqrt(rms3 / (3.0 * count));
+        if (rms3 == 0) rms3 = 1;
+        double s3 = std::sqrt(3.0) / rms3;
+        cv::Matx44d U(s3, 0, 0, -s3 * c3[0],
+                      0, s3, 0, -s3 * c3[1],
+                      0, 0, s3, -s3 * c3[2],
+                      0, 0, 0, 1);
+        cv::Matx44d Uinv(1.0 / s3, 0, 0, c3[0],
+                         0, 1.0 / s3, 0, c3[1],
+                         0, 0, 1.0 / s3, c3[2],
+                         0, 0, 0, 1);
+
+        std::vector<cv::Vec3d> xs_n(count);
+        cv::Vec2d c2(0, 0);
+        for (int i = 0; i < count; ++i) {
+            double w = xs_in[i][2];
+            c2 += cv::Vec2d(xs_in[i][0] / w, xs_in[i][1] / w);
+        }
+        c2 *= (1.0 / count);
+        double rms2 = 0;
+        for (int i = 0; i < count; ++i) {
+            double w = xs_in[i][2];
+            cv::Vec2d d(xs_in[i][0] / w - c2[0], xs_in[i][1] / w - c2[1]);
+            rms2 += d.dot(d);
+        }
+        rms2 = std::sqrt(rms2 / (2.0 * count));
+        if (rms2 == 0) rms2 = 1;
+        double s2 = std::sqrt(2.0) / rms2;
+        cv::Matx33d T(s2, 0, -s2 * c2[0],
+                      0, s2, -s2 * c2[1],
+                      0, 0, 1);
+        cv::Matx33d Tinv(1.0 / s2, 0, c2[0],
+                         0, 1.0 / s2, c2[1],
+                         0, 0, 1);
+
+        std::vector<cv::Vec3d> Xs_n(count);
+        for (int i = 0; i < count; ++i) {
+            cv::Vec4d Xh(Xs_in[i][0], Xs_in[i][1], Xs_in[i][2], 1);
+            cv::Vec4d Xn = U * Xh;
+            Xs_n[i] = cv::Vec3d(Xn[0], Xn[1], Xn[2]);
+            xs_n[i] = T * xs_in[i];
+        }
 
         mat A(2 * count, 12);
         A.setZero();
 
         for (int i = 0; i < count; ++i) {
-            double x = xs[i][0], y = xs[i][1], w = xs[i][2];
-            double X = Xs[i][0], Y = Xs[i][1], Z = Xs[i][2];
+            double x = xs_n[i][0], y = xs_n[i][1], w = xs_n[i][2];
+            double X = Xs_n[i][0], Y = Xs_n[i][1], Z = Xs_n[i][2];
             double W = 1.0;
 
-            // eq1: y*(P3*X) - w*(P2*X) = 0
             A(2 * i,     4)  = -w * X; A(2 * i,     5)  = -w * Y; A(2 * i,     6)  = -w * Z; A(2 * i,     7)  = -w * W;
             A(2 * i,     8)  =  y * X; A(2 * i,     9)  =  y * Y; A(2 * i,     10) =  y * Z; A(2 * i,     11) =  y * W;
-            // eq2: w*(P1*X) - x*(P3*X) = 0
             A(2 * i + 1, 0)  =  w * X; A(2 * i + 1, 1)  =  w * Y; A(2 * i + 1, 2)  =  w * Z; A(2 * i + 1, 3)  =  w * W;
             A(2 * i + 1, 8)  = -x * X; A(2 * i + 1, 9)  = -x * Y; A(2 * i + 1, 10) = -x * Z; A(2 * i + 1, 11) = -x * W;
         }
@@ -71,12 +119,29 @@ namespace {
         Eigen::JacobiSVD<mat> svd(A, Eigen::ComputeFullV);
         Eigen::VectorXd p = svd.matrixV().col(11);
 
-        matrix34d result;
+        matrix34d P_n;
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 4; ++j)
-                result(i, j) = p[i * 4 + j];
+                P_n(i, j) = p[i * 4 + j];
 
-        return canonicalizeP(result);
+        matrix34d P_nU;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                double s = 0;
+                for (int k = 0; k < 4; ++k) s += P_n(i, k) * U(k, j);
+                P_nU(i, j) = s;
+            }
+        }
+        matrix34d P_orig;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                double s = 0;
+                for (int k = 0; k < 3; ++k) s += Tinv(i, k) * P_nU(k, j);
+                P_orig(i, j) = s;
+            }
+        }
+
+        return canonicalizeP(P_orig);
     }
 
     // По трехмерным точкам и их проекциям на изображении определяем положение камеры
@@ -133,47 +198,6 @@ namespace {
 
         if (best_support == 0) {
             throw std::runtime_error("estimateCameraMatrixRANSAC : failed to estimate camera matrix");
-        }
-
-        cv::Mat K_mat(calib.K());
-        cv::Mat R_init(3, 3, CV_64F);
-        for (int r = 0; r < 3; r++)
-            for (int c = 0; c < 3; c++)
-                R_init.at<double>(r, c) = best_P(r, c);
-        cv::Mat rvec, tvec(3, 1, CV_64F);
-        cv::Rodrigues(R_init, rvec);
-        tvec.at<double>(0) = best_P(0, 3);
-        tvec.at<double>(1) = best_P(1, 3);
-        tvec.at<double>(2) = best_P(2, 3);
-
-        for (double inlier_thr : {threshold_px, threshold_px * 0.5, threshold_px * 0.25}) {
-            std::vector<cv::Point3d> obj_pts;
-            std::vector<cv::Point2d> img_pts;
-
-            cv::Mat R_cur;
-            cv::Rodrigues(rvec, R_cur);
-            matrix34d P_cur;
-            for (int r = 0; r < 3; r++) {
-                for (int c = 0; c < 3; c++) P_cur(r, c) = R_cur.at<double>(r, c);
-                P_cur(r, 3) = tvec.at<double>(r);
-            }
-            for (int i = 0; i < n_points; ++i) {
-                cv::Vec3d px_h = calib.project(P_cur * cv::Vec4d(X[i][0], X[i][1], X[i][2], 1));
-                cv::Vec2d px = {px_h[0] / px_h[2], px_h[1] / px_h[2]};
-                if (cv::norm(px - x[i]) < inlier_thr) {
-                    obj_pts.push_back({X[i][0], X[i][1], X[i][2]});
-                    img_pts.push_back({x[i][0], x[i][1]});
-                }
-            }
-            if (obj_pts.size() < 6) break;
-            cv::solvePnP(obj_pts, img_pts, K_mat, cv::noArray(), rvec, tvec, true, cv::SOLVEPNP_ITERATIVE);
-        }
-
-        cv::Mat R_refined;
-        cv::Rodrigues(rvec, R_refined);
-        for (int r = 0; r < 3; r++) {
-            for (int c = 0; c < 3; c++) best_P(r, c) = R_refined.at<double>(r, c);
-            best_P(r, 3) = tvec.at<double>(r);
         }
 
         return best_P;
