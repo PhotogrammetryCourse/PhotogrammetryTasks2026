d9ab11c4c8b4a11690676230a6150c3c8a398dc9
Timur <timursyrma@example.org>
2026-05-08 14:46:07 +0300

    threshold=5, wide_threshold=10

--- src/phg/sfm/resection.cpp
+++ src/phg/sfm/resection.cpp
@@ -47,67 +47,17 @@ namespace {
         return result;
     }
 
-    cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs_in, const cv::Vec3d *xs_in, int count)
+    cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs, const cv::Vec3d *xs, int count)
     {
         using mat = Eigen::MatrixXd;
-
-        cv::Vec3d c3(0, 0, 0);
-        for (int i = 0; i < count; ++i) c3 += Xs_in[i];
-        c3 *= (1.0 / count);
-        double rms3 = 0;
-        for (int i = 0; i < count; ++i) {
-            cv::Vec3d d = Xs_in[i] - c3;
-            rms3 += d.dot(d);
-        }
-        rms3 = std::sqrt(rms3 / (3.0 * count));
-        if (rms3 == 0) rms3 = 1;
-        double s3 = std::sqrt(3.0) / rms3;
-        cv::Matx44d U(s3, 0, 0, -s3 * c3[0],
-                      0, s3, 0, -s3 * c3[1],
-                      0, 0, s3, -s3 * c3[2],
-                      0, 0, 0, 1);
-        cv::Matx44d Uinv(1.0 / s3, 0, 0, c3[0],
-                         0, 1.0 / s3, 0, c3[1],
-                         0, 0, 1.0 / s3, c3[2],
-                         0, 0, 0, 1);
-
-        std::vector<cv::Vec3d> xs_n(count);
-        cv::Vec2d c2(0, 0);
-        for (int i = 0; i < count; ++i) {
-            double w = xs_in[i][2];
-            c2 += cv::Vec2d(xs_in[i][0] / w, xs_in[i][1] / w);
-        }
-        c2 *= (1.0 / count);
-        double rms2 = 0;
-        for (int i = 0; i < count; ++i) {
-            double w = xs_in[i][2];
-            cv::Vec2d d(xs_in[i][0] / w - c2[0], xs_in[i][1] / w - c2[1]);
-            rms2 += d.dot(d);
-        }
-        rms2 = std::sqrt(rms2 / (2.0 * count));
-        if (rms2 == 0) rms2 = 1;
-        double s2 = std::sqrt(2.0) / rms2;
-        cv::Matx33d T(s2, 0, -s2 * c2[0],
-                      0, s2, -s2 * c2[1],
-                      0, 0, 1);
-        cv::Matx33d Tinv(1.0 / s2, 0, c2[0],
-                         0, 1.0 / s2, c2[1],
-                         0, 0, 1);
-
-        std::vector<cv::Vec3d> Xs_n(count);
-        for (int i = 0; i < count; ++i) {
-            cv::Vec4d Xh(Xs_in[i][0], Xs_in[i][1], Xs_in[i][2], 1);
-            cv::Vec4d Xn = U * Xh;
-            Xs_n[i] = cv::Vec3d(Xn[0], Xn[1], Xn[2]);
-            xs_n[i] = T * xs_in[i];
-        }
+        using vec = Eigen::VectorXd;
 
         mat A(2 * count, 12);
         A.setZero();
 
         for (int i = 0; i < count; ++i) {
-            double x = xs_n[i][0], y = xs_n[i][1], w = xs_n[i][2];
-            double X = Xs_n[i][0], Y = Xs_n[i][1], Z = Xs_n[i][2];
+            double x = xs[i][0], y = xs[i][1], w = xs[i][2];
+            double X = Xs[i][0], Y = Xs[i][1], Z = Xs[i][2];
             double W = 1.0;
 
             A(2 * i,     4)  = -w * X; A(2 * i,     5)  = -w * Y; A(2 * i,     6)  = -w * Z; A(2 * i,     7)  = -w * W;
@@ -117,31 +67,14 @@ namespace {
         }
 
         Eigen::JacobiSVD<mat> svd(A, Eigen::ComputeFullV);
-        Eigen::VectorXd p = svd.matrixV().col(11);
+        vec p = svd.matrixV().col(11);
 
-        matrix34d P_n;
-        for (int i = 0; i < 3; ++i)
-            for (int j = 0; j < 4; ++j)
-                P_n(i, j) = p[i * 4 + j];
-
-        matrix34d P_nU;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                double s = 0;
-                for (int k = 0; k < 4; ++k) s += P_n(i, k) * U(k, j);
-                P_nU(i, j) = s;
-            }
-        }
-        matrix34d P_orig;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                double s = 0;
-                for (int k = 0; k < 3; ++k) s += Tinv(i, k) * P_nU(k, j);
-                P_orig(i, j) = s;
-            }
+        matrix34d result;
+        for (int i = 0; i < 12; ++i) {
+            result(i / 4, i % 4) = p(i);
         }
 
-        return canonicalizeP(P_orig);
+        return canonicalizeP(result);
     }
 
     // По трехмерным точкам и их проекциям на изображении определяем положение камеры
@@ -153,13 +86,21 @@ namespace {
 
         const int n_points = X.size();
         const int n_trials = 10000;
-        const double threshold_px = 8;
-        const double scoring_threshold_px = 4;
+        const double threshold_px = 5;
         const int n_samples = 6;
         uint64_t seed = 1;
 
-        const double T2 = scoring_threshold_px * scoring_threshold_px;
-        double best_cost = std::numeric_limits<double>::max();
+        auto countSupport = [&](const cv::Matx34d &P) -> int {
+            int s = 0;
+            for (int i = 0; i < n_points; ++i) {
+                cv::Vec3d px_h = calib.project(P * cv::Vec4d(X[i][0], X[i][1], X[i][2], 1));
+                if (px_h[2] == 0) continue;
+                cv::Vec2d px(px_h[0] / px_h[2], px_h[1] / px_h[2]);
+                if (cv::norm(px - x[i]) < threshold_px) ++s;
+            }
+            return s;
+        };
+
         int best_support = 0;
         cv::Matx34d best_P;
 
@@ -175,37 +116,45 @@ namespace {
             }
 
             cv::Matx34d P = estimateCameraMatrixDLT(ms0, ms1, n_samples);
+            int support = countSupport(P);
 
-            double cost = 0;
-            int support = 0;
-            for (int i = 0; i < n_points; ++i) {
-                cv::Vec3d px_h = calib.project(P * cv::Vec4d(X[i][0], X[i][1], X[i][2], 1));
-                cv::Vec2d px = {px_h[0] / px_h[2], px_h[1] / px_h[2]};
-                cv::Vec2d d = px - x[i];
-                double e2 = d.dot(d);
-                if (e2 < T2) {
-                    cost += e2;
-                } else {
-                    cost += T2;
-                }
-                if (e2 < threshold_px * threshold_px) ++support;
-            }
-
-            if (cost < best_cost) {
-                best_cost = cost;
+            if (support > best_support) {
                 best_support = support;
                 best_P = P;
-
-                std::cout << "estimateCameraMatrixRANSAC : MSAC cost: " << best_cost << " support: " << best_support << "/" << n_points << std::endl;
+                std::cout << "estimateCameraMatrixRANSAC : support: " << best_support << "/" << n_points << std::endl;
+                if (best_support == n_points) break;
             }
         }
-
-        std::cout << "estimateCameraMatrixRANSAC : best support: " << best_support << "/" << n_points << std::endl;
 
         if (best_support == 0) {
             throw std::runtime_error("estimateCameraMatrixRANSAC : failed to estimate camera matrix");
         }
 
+        const double wide_threshold = 2.0 * threshold_px;
+        for (int refine_iter = 0; refine_iter < 10; ++refine_iter) {
+            std::vector<cv::Vec3d> Xs_in;
+            std::vector<cv::Vec3d> xs_in;
+            for (int i = 0; i < n_points; ++i) {
+                cv::Vec3d px_h = calib.project(best_P * cv::Vec4d(X[i][0], X[i][1], X[i][2], 1));
+                if (px_h[2] == 0) continue;
+                cv::Vec2d px(px_h[0] / px_h[2], px_h[1] / px_h[2]);
+                if (cv::norm(px - x[i]) < wide_threshold) {
+                    Xs_in.push_back(X[i]);
+                    xs_in.push_back(calib.unproject(x[i]));
+                }
+            }
+            if ((int)Xs_in.size() <= n_samples) break;
+
+            cv::Matx34d P_refit = estimateCameraMatrixDLT(Xs_in.data(), xs_in.data(), (int)Xs_in.size());
+            int new_support = countSupport(P_refit);
+            if (new_support <= best_support) break;
+
+            best_support = new_support;
+            best_P = P_refit;
+            std::cout << "estimateCameraMatrixRANSAC refine: support: " << best_support << "/" << n_points << std::endl;
+        }
+
+        std::cout << "estimateCameraMatrixRANSAC : best support: " << best_support << "/" << n_points << std::endl;
         return best_P;
     }
 
